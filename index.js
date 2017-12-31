9103dfe9bdc3380c2b239116af80b2832757bd15
Jonathan Valdesuso <jbvaldesuso@gmail.com>
2017-12-31 18:55:54 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('div#nested div.target'); 
+}
+
+function increaseRankBy(n) {
+  const rankedList = document.querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < rankedList.length; i++) {
+    rankedList[i].innerHTML = (parseInt(rankedList[i].innerHTML) + n).toString();
+  }
+}
+
+function deepestChild() {
+  const grandNode = document.querySelectorAll('div#grand-node div');  
+  var deepestChild;
+  for (let i = 0; i < grandNode.length - 1; i++) {
+    deepestChild = grandNode[i].querySelector('div');
+  }
+  return deepestChild;
+}
